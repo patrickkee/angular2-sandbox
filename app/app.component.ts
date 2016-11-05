489edf3330443b37112d9abce58d907ff85699e7
Patrick Kee <patrick.kee0@gmail.com>
2016-11-05 14:45:17 -0400

    added some interactivity and conditional attribute display

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,15 +4,36 @@ import { Component, ElementRef } from '@angular/core';
     selector: 'my-app',
     template: `
     	<h1>Hello Angular!</h1>
+    	<p>Application Context Foo Value: <strong>{{appCtxt.foo}}</strong></p>
     	<child></child>
-	    <div>
-	      <pre>{{ data | json }}</pre>
-	    </div>
+    	<br/><br/>
+	    <button (click)="toggleInteractive()">Interactive Mode</button>
+	    <br/><br/>
+	    <div *ngIf="appCtxt.interactive==true">
+		    <textarea rows="1" (keyup)="onkeyup($event)">{{echoTxt}}</textarea>
+		    <button (click)="onClickMe()">Alert Me!</button>
+		    <p>{{echoTxt}}</p>
+		  </div>
     `
 })
 export class AppComponent { 
-  data: any;
+  appCtxt: any;
+  echoTxt: string;
   constructor() {
-    this.data = window.appCtxt;
+    this.appCtxt = window.appCtxt;
+    this.appCtxt.interactive = false;
+    this.echoTxt = "";
+  }
+
+  onClickMe() {
+  	alert(this.echoTxt);
+  }
+
+  onkeyup(event:any) {
+  	this.echoTxt = event.target.value;
+  }
+
+  toggleInteractive() {
+  	this.appCtxt.interactive = !this.appCtxt.interactive;
   }
 }
