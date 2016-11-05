33f04c78f163f4f7d2bc7ae894fafb07d93a5405
Patrick Kee <patrick.kee0@gmail.com>
2016-11-04 23:54:38 -0400

    initial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular!</h1>`
+    template: `
+    	<h1>Hello Angular!</h1>
+    	<child></child>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+	constructor(elementRef: ElementRef) {
+  	console.log(elementRef.nativeElement.getAttribute("appCtxt"));
+  }
+}
---
+++ app/child.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector:'child',
+	template: `
+		<label>child text</label>
+		<textarea></textarea>
+	`
+})
+export class Child {
+
+}
